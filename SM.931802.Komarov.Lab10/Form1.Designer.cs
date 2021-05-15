be135a930c8250956912143bf30abd309e24b7c0
GPLuna <gpluna@mail.ru>
2021-05-15 16:04:25 +0700

    Добавьте файлы проекта.

---
+++ SM.931802.Komarov.Lab10/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿
+namespace SM._931802.Komarov.Lab10
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PLabel = new System.Windows.Forms.Label();
+            this.CLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.Label();
+            this.PRoll1 = new System.Windows.Forms.NumericUpDown();
+            this.PRoll2 = new System.Windows.Forms.NumericUpDown();
+            this.PRoll3 = new System.Windows.Forms.NumericUpDown();
+            this.PRoll4 = new System.Windows.Forms.NumericUpDown();
+            this.CRoll1 = new System.Windows.Forms.NumericUpDown();
+            this.CRoll2 = new System.Windows.Forms.NumericUpDown();
+            this.CRoll3 = new System.Windows.Forms.NumericUpDown();
+            this.CRoll4 = new System.Windows.Forms.NumericUpDown();
+            this.PSum = new System.Windows.Forms.NumericUpDown();
+            this.CSum = new System.Windows.Forms.NumericUpDown();
+            this.CScore = new System.Windows.Forms.NumericUpDown();
+            this.PScore = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PRoll1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PRoll2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PRoll3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PRoll4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CRoll1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CRoll2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CRoll3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CRoll4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PSum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CSum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CScore)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PScore)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PLabel
+            // 
+            this.PLabel.AutoSize = true;
+            this.PLabel.Location = new System.Drawing.Point(197, 142);
+            this.PLabel.Name = "PLabel";
+            this.PLabel.Size = new System.Drawing.Size(39, 15);
+            this.PLabel.TabIndex = 8;
+            this.PLabel.Text = "Player";
+            // 
+            // CLabel
+            // 
+            this.CLabel.AutoSize = true;
+            this.CLabel.Location = new System.Drawing.Point(529, 142);
+            this.CLabel.Name = "CLabel";
+            this.CLabel.Size = new System.Drawing.Size(61, 15);
+            this.CLabel.TabIndex = 9;
+            this.CLabel.Text = "Computer";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(332, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 47);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Roll";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.ForeColor = System.Drawing.Color.Red;
+            this.Result.Location = new System.Drawing.Point(346, 250);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(0, 15);
+            this.Result.TabIndex = 13;
+            // 
+            // PRoll1
+            // 
+            this.PRoll1.Location = new System.Drawing.Point(163, 174);
+            this.PRoll1.Name = "PRoll1";
+            this.PRoll1.Size = new System.Drawing.Size(120, 23);
+            this.PRoll1.TabIndex = 14;
+            // 
+            // PRoll2
+            // 
+            this.PRoll2.Location = new System.Drawing.Point(163, 203);
+            this.PRoll2.Name = "PRoll2";
+            this.PRoll2.Size = new System.Drawing.Size(120, 23);
+            this.PRoll2.TabIndex = 15;
+            // 
+            // PRoll3
+            // 
+            this.PRoll3.Location = new System.Drawing.Point(163, 232);
+            this.PRoll3.Name = "PRoll3";
+            this.PRoll3.Size = new System.Drawing.Size(120, 23);
+            this.PRoll3.TabIndex = 16;
+            // 
+            // PRoll4
+            // 
+            this.PRoll4.Location = new System.Drawing.Point(163, 261);
+            this.PRoll4.Name = "PRoll4";
+            this.PRoll4.Size = new System.Drawing.Size(120, 23);
+            this.PRoll4.TabIndex = 17;
+            // 
+            // CRoll1
+            // 
+            this.CRoll1.Location = new System.Drawing.Point(505, 174);
+            this.CRoll1.Name = "CRoll1";
+            this.CRoll1.Size = new System.Drawing.Size(120, 23);
+            this.CRoll1.TabIndex = 18;
+            // 
+            // CRoll2
+            // 
+            this.CRoll2.Location = new System.Drawing.Point(505, 203);
+            this.CRoll2.Name = "CRoll2";
+            this.CRoll2.Size = new System.Drawing.Size(120, 23);
+            this.CRoll2.TabIndex = 19;
+            // 
+            // CRoll3
+            // 
+            this.CRoll3.Location = new System.Drawing.Point(505, 232);
+            this.CRoll3.Name = "CRoll3";
+            this.CRoll3.Size = new System.Drawing.Size(120, 23);
+            this.CRoll3.TabIndex = 20;
+            // 
+            // CRoll4
+            // 
+            this.CRoll4.Location = new System.Drawing.Point(505, 261);
+            this.CRoll4.Name = "CRoll4";
+            this.CRoll4.Size = new System.Drawing.Size(120, 23);
+            this.CRoll4.TabIndex = 21;
+            // 
+            // PSum
+            // 
+            this.PSum.Location = new System.Drawing.Point(163, 303);
+            this.PSum.Name = "PSum";
+            this.PSum.Size = new System.Drawing.Size(120, 23);
+            this.PSum.TabIndex = 22;
+            // 
+            // CSum
+            // 
+            this.CSum.Location = new System.Drawing.Point(505, 303);
+            this.CSum.Name = "CSum";
+            this.CSum.Size = new System.Drawing.Size(120, 23);
+            this.CSum.TabIndex = 23;
+            // 
+            // CScore
+            // 
+            this.CScore.Location = new System.Drawing.Point(505, 366);
+            this.CScore.Name = "CScore";
+            this.CScore.Size = new System.Drawing.Size(120, 23);
+            this.CScore.TabIndex = 24;
+            // 
+            // PScore
+            // 
+            this.PScore.Location = new System.Drawing.Point(163, 366);
+            this.PScore.Name = "PScore";
+            this.PScore.Size = new System.Drawing.Size(120, 23);
+            this.PScore.TabIndex = 25;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(197, 339);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 15);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Score";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(543, 339);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Score";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PScore);
+            this.Controls.Add(this.CScore);
+            this.Controls.Add(this.CSum);
+            this.Controls.Add(this.PSum);
+            this.Controls.Add(this.CRoll4);
+            this.Controls.Add(this.CRoll3);
+            this.Controls.Add(this.CRoll2);
+            this.Controls.Add(this.CRoll1);
+            this.Controls.Add(this.PRoll4);
+            this.Controls.Add(this.PRoll3);
+            this.Controls.Add(this.PRoll2);
+            this.Controls.Add(this.PRoll1);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CLabel);
+            this.Controls.Add(this.PLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PRoll1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PRoll2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PRoll3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PRoll4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CRoll1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CRoll2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CRoll3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CRoll4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PSum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CSum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CScore)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PScore)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label PLabel;
+        private System.Windows.Forms.Label CLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.NumericUpDown PRoll1;
+        private System.Windows.Forms.NumericUpDown PRoll2;
+        private System.Windows.Forms.NumericUpDown PRoll3;
+        private System.Windows.Forms.NumericUpDown PRoll4;
+        private System.Windows.Forms.NumericUpDown CRoll1;
+        private System.Windows.Forms.NumericUpDown CRoll2;
+        private System.Windows.Forms.NumericUpDown CRoll3;
+        private System.Windows.Forms.NumericUpDown CRoll4;
+        private System.Windows.Forms.NumericUpDown PSum;
+        private System.Windows.Forms.NumericUpDown CSum;
+        private System.Windows.Forms.NumericUpDown CScore;
+        private System.Windows.Forms.NumericUpDown PScore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SM.931802.Komarov.Lab10/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SM._931802.Komarov.Lab10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Dice Player = new Dice();
+        Dice Comp = new Dice();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] array = Player.GenerateForPlayer();
+            PRoll1.Text = array[0].ToString();
+            PRoll2.Text = array[1].ToString();
+            PRoll3.Text = array[2].ToString();
+            PRoll4.Text = array[3].ToString();
+            array = Comp.GenerateForComputer();
+            CRoll1.Text = array[0].ToString();
+            CRoll2.Text = array[1].ToString();
+            CRoll3.Text = array[2].ToString();
+            CRoll4.Text = array[3].ToString();
+            PSum.Value = (PRoll1.Value + PRoll2.Value + PRoll3.Value + PRoll4.Value);
+            CSum.Value = (CRoll1.Value + CRoll2.Value + CRoll3.Value + CRoll4.Value);
+            if ((PSum.Value) > (CSum.Value))
+            {
+                Result.Text = "Player wins";
+                PScore.Value += 1;
+            }
+            else if ((CSum.Value) > (PSum.Value))
+            {
+                Result.Text = "Computer wins";
+                CScore.Value += 1;
+            }
+            else if ((CSum.Value) == (PSum.Value))
+            {
+                Result.Text = "Draw";
+            }
+
+        }
+
+        public class Dice
+        {
+            int[] number = new int[6] { 1, 2, 3, 4, 5, 6 };
+            Random r = new Random();
+            public int[] GenerateForPlayer()
+            {
+                decimal[] prob = new decimal[6] { 0.16m, 0.17m, 0.17m, 0.17m, 0.17m, 0.17m };
+                int[] array = new int[4];
+                int k;
+                decimal A;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    A = (decimal)r.NextDouble();
+                    for (k = -1; A > 0; k++)
+                    {
+                        A -= prob[k + 1];
+                    }
+                    array[i] = number[k];
+                }
+                return array;
+            }
+            public int[] GenerateForComputer()
+            {
+                decimal[] prob = new decimal[6] { 0.1m, 0.1m, 0.1m, 0.2m, 0.2m, 0.3m };
+                int[] array = new int[4];
+                int k;
+                decimal A;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    A = (decimal)r.NextDouble();
+                    for (k = -1; A > 0; k++)
+                    {
+                        A -= prob[k + 1];
+                    }
+                    array[i] = number[k];
+                }
+                return array;
+            }
+        }
+    }
+}
